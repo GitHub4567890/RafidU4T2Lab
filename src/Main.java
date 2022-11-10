c215e3502c05cb9ada5efa3e6a116a85283f414f
Ibtehaz Rafid <IbtehazR@nycstudents.net>
2022-11-10 12:34:23 -0500

    1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,84 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 4; i < 15; i += 2) {
+          System.out.print(i + " ");
+        }
+
+
+    }
 }
+
+
+//for (int i = 4; i < 12; i++) {
+//        System.out.print(i + " ");
+//        }
+
+
+//       for (int i = 4; i <= 14; i += 2) {
+//          System.out.print(i + " ");
+//          }
+
+
+//    int i;
+//    int j = 40;
+//        for (i = 10; i <= j + 10; i += 5) {
+//                System.out.print(i + " ");
+//                }
+
+
+//    int i = 20;
+//        while (i >= 0) {
+//                System.out.print(i + " ");
+//                i -= 3;
+//                }
+
+
+//    int i = 5;
+//        while (i < 10) {
+//                System.out.print(i + " ");
+//                i++;
+//                }
+
+
+
+//        for (int count = 5; count < 10; count++) {
+//        System.out.print(count + " ");
+//        }
+//        System.out.println("");
+//        for (int i = 20; i >= 0; i = i - 3) {
+//        System.out.print(i + " ");
+//        }
+//        System.out.println("");
+//        int sum = 0;
+//        int x = 5;
+//        for (int i = 0; i < 10 + x; i += 2) {
+//        sum += i;
+//        }
+//        System.out.println(sum);
+
+
+//        for (int x = 1; x <= 10; x++) {
+//                System.out.print(x + " ");
+//                }
+//                System.out.println("");
+//                for (int x = 10; x >= 1; x--) {
+//                System.out.print(x + " ");
+//                }
+//                System.out.println("");
+//                for (int x = 15; x >= 1; x--) {
+//                System.out.print(x + " ");
+//                }
+//                System.out.println("");
+//                for (int x = 1; x <= 15; x++) {
+//                System.out.print(x + " ");
+//                }
+//                System.out.println();
+//                for (int x = 1; x <= 15; x++) {
+//                if (x % 2 != 0) {
+//                System.out.print(x + " ");
+//                }
+//                }
+//                System.out.println();
+//                for (int x = 1; x <= 15; x += 2) {
+//                System.out.print(x + " ");
+//                }
